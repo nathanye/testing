b28ab38d043e3e96479b4cea61a8aa5b4457ddd2
nathanye <khalil54781@gmail.com>
2017-07-29 21:44:27 +0800

    run year final_refactor

--- leap_year.rb
+++ leap_year.rb
@@ -1,16 +1,3 @@
-def is_leap_year?(year)
-  if year % 4 == 0
-    if year % 100 == 0
-      if year % 400 == 0
-        return true
-      else
-          return false
-      end    
-      return false
-    else
-      return true
-    end
-  else
-    return false
-  end
+def is_leap_year?(y)
+  y % 400 == 0 || ( y % 4 == 0 && y % 100 != 0 )
 end
